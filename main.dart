4572dd5f2788783c266a5c150ca3cde771c95d42
Tensor-Programming <abeltensor@tensor-programming.com>
2019-02-04 22:35:17 -0500

    Part-2

--- main.dart
+++ main.dart
@@ -1,131 +1,111 @@
 // void main() {
-//   print("Hello, World");
-// }
-
-// void main() {
-//   print("Hi");
-//   print("From Dart!");
-// }
-
-// void main() {
-//   var x = -10;
-
-//   print(x.abs());
-// }
-
-// void main() {
-// Numbers - (int, double) num
-// Strings - "Hello!" (single and double quotes)
-// Booleans - true or false
-// Lists - collections of items (like arrays) List<int> 0 indexed
-// Maps - Collections with associated Key Value Pairs Map<String, int>
-// runes - unicode character points
-// symbols - #symbol (simbolic metadata)
-
-//   int x = 10;
-//   double y = 10.0;
-
-//   String s = "${x + y}";
-//   print(s);
-//   bool b = true;
-//   print(b);
-//   List l = [1, 2, 3];
-//   print(l[0]);
-//   List<String> ls = ["1", "2", "3"];
-//   print(ls[1]);
-
-//   Map<String, int> map = {
-//     'A': 10,
-//     'B': 20,
-//     'C': 30,
-//   };
-
-//   print(map["A"]);
-// }
-
-// int add(int a, int b) {
-//   return a + b;
-// }
-// Type based function
-// add(a, b) {
-//   return a + b;
-// }
-// No types
-
-// void main() {
-//   print(add(1, 2));
-//   print(add(20.0, 40.0));
-//   print(add("a", "b"));
-//   print(add(true, false));
-// }
-
-// int add(int a, int b) {
-//   return a + b;
-// }
-
-// Function fun;
-
-// void main() {
-//   fun = add;
-
-//   var result = fun(20, 30);
-
-//   print("Result is $result");
-// }
-
-// int add(int a, int b) {
-//   return a + b;
-// }
-
-// exec(Function op, x, y) {
-//   return op(x, y);
-// }
-
-// void main() {
-//   var result = exec(add, 20, 30);
-//   print("Result is $result");
-// }
-
-// int add(int x, int y) => x + y;
-// int sub(int x, int y) => x + y;
-
-// choose(bool op) {
-//   if (op == true) {
-//     return add;
-//   } else {
-//     return sub;
+//   var y;
+//   for (int x = 10; x > 1; x--) {
+//     y = x + 1;
+//     print("$x");
 //   }
+
+//   print(y);
 // }
 
 // void main() {
-//   var result = choose(true)(10, 20);
-//   print("Result is $result");
+//   int x = 10;
+//   do {
+//     print("$x");
+//     x--;
+//   } while (x > 1);
+// int x = 10;
+// while (x > 1) {
+//   print("$x");
+//   x--;
 // }
+//   var digit = 1;
 
-// int add(int x, int y) => x + y;
-// int sub(int x, int y) => x + y;
+//   switch (digit) {
+//     case 0:
+//       print("zero");
+//       break;
+//     case 1:
+//     case 2:
+//     case 3:
+//     case 4:
+//       print("four");
+//       break;
+//     default:
+//       print("not a digit");
+//   }
 
-// List<Function> operators = [add, sub];
+// var s = "Circle";
+// switch (s) {
+//   case "Square":
+//     print("4 sides");
+//     break;
+//   case "Rectangle":
+//     print("4 sides with 2 different lengths");
+//     break;
+//   case "Circle":
+//     print("Round and Round we go");
+//     break;
+//   default:
+//     print("No match");
+// }
+// }
 
 // void main() {
-//   var result = operators[1](10, 20);
-//   print("Result is $result");
-// }
+//   bool t = true;
+//   bool f = false;
 
-// calc(int b) {
-//   int c = 1;
+//   if (!f) {
+//     print("Negate False");
+//   } else {
+//     print("Will never print");
+//   }
 
-//   return () => print("The value is ${b + c++}");
+//   if (1 < 10) {
+//     print("1 is less than 10");
+//   } else if (1 >= 0) {
+//     print("1 is greater than or equal to 0");
+//   } else if (-1 < 0) {
+//     print("Will never print");
+//   }
+
+// == (= =) equal to
+// != (! =) not equal to
+// > greater than
+// < less than
+// >= (> =) greater than or equal to
+// <= (< =) less than or equal to
+// ! negation
 // }
 
 // void main() {
-//   (a, b) {
-//     print("Hello, from closure: ${a + b}");
-//   }(20, 30.0);
+//   print("This is a String");
+//   print('This is a String');
 
-//   var f = calc(10);
-//   f();
-//   calc(10)();
-//   f();
-//   f();
+//   var x = 1.4;
+//   var y = 2;
+//   var a = "\"Add numbers: \n\n $x and $y: \$${x + y}\"";
+//   print(a);
 // }
+
+// void main() {
+//   var x = [
+//     1,
+//     "2",
+//     3.0,
+//     true,
+//     () {},
+//     #symbol,
+//     Runes("123"),
+//     [1, 2, 3],
+//     {"A": 4}
+//   ];
+
+//   //dynamic -> Generalizes all Types
+//   //Object -> All Types are derived from Object
+
+//   print(x);
+// }
+
+// JIT (Just in Time) -- Hot Reloading
+// AOT (Ahead of Time) -- Publishing Program
